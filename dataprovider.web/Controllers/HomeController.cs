eee62bd286c656a0addaad7b737d557407e9be96
zhq <zhq_092012@163.com>
2019-12-14 11:26:32 +0800

    添加welcom配置页

--- dataprovider.web/Controllers/HomeController.cs
+++ dataprovider.web/Controllers/HomeController.cs
@@ -10,32 +10,22 @@ using Microsoft.Extensions.Configuration;
 
 namespace dataprovider.web.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    private readonly ILogger<HomeController> _logger;
+    private readonly IConfiguration _configuration;
+
+    public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
     {
-        private readonly ILogger<HomeController> _logger;
-        private readonly IConfiguration _configuration;
-
-        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
-        {
-            _logger = logger;
-            _configuration = configuration;
-        }
-
-        public IActionResult Index()
-        {
-            ViewData["message"] = _configuration["Message"];
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+      _logger = logger;
+      _configuration = configuration;
     }
+
+    public IActionResult Index()
+    {
+      ViewData["message"] = _configuration["Message"];
+      return View();
+    }
+
+  }
 }
