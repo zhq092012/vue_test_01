410bb28613407cc9404649f7f7a095e2d098026a
zhq092012 <zhq_092012@163.com>
2019-12-16 23:30:19 +0800

    添加视图组件

--- dataprovider.Models/ProvContext.cs
+++ dataprovider.Models/ProvContext.cs
@@ -4,21 +4,12 @@ using Microsoft.EntityFrameworkCore;
 
 namespace dataprovider.Models
 {
-  public class ProvContext : DbContext
-  {
-    //protected ProvContext()
-    //{
-    //}
-
-    public ProvContext(DbContextOptions options) : base(options)
+    public class ProvContext : DbContext
     {
+        public ProvContext(DbContextOptions options) : base(options)
+        {
+        }
+        public virtual DbSet<Student> Students { get; set; }
+        public virtual DbSet<TbBsPplb> TbBsPplb { get; set; }
     }
-    public virtual DbSet<Student> Students { get; set; }
-    public virtual DbSet<TbBsPplb> TbBsPplb { get; set; }
-
-    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    //{
-    //  optionsBuilder.UseMySql("server=localhost;port=3306;uid=sa;pwd=zhq092012;database=MyDataDB");
-    //}
-  }
 }
---
+++ dataprovider.Web/ViewComponents/WelcomeViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using dataprovider.Models;
+using dataprovider.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dataprovider.Web.ViewComponents
+{
+    /// <summary>
+    /// 添加视图组件，相当与一个小型的MVC
+    /// </summary>
+    public class WelcomeViewComponent:ViewComponent
+    {
+        private readonly IRepository<Student> repository;
+
+        public WelcomeViewComponent(IRepository<Student> repository)
+        {
+            this.repository = repository;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var count = repository.GetAll().Count();
+            return View("Default",count.ToString());
+        }
+    }
+}
